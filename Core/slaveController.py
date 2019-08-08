b7185b44df693c7938c836f822cdabf887c50260
Diego Boccoli Gallego <diego.gallego@flashapp.com.br>
2019-08-08 07:06:15 -0300

    adding logic of input/output

--- Core/slaveController.py
+++ Core/slaveController.py
@@ -1,33 +1,29 @@
 from threading import Thread
+import RPi.GPIO as GPIO 
 
 voltageMaps = {
     'HIGH': GPIO.HIGH,
     'LOW': GPIO.LOW
 }
 
-def moduleExists(moduleName):
-    try:
-        __import__(moduleName)
-    except ImportError:
-        return False
-    else:
-        return True    
-
 class SlaveController(Thread):
     def __init__(self):
-        Thread.__init__(self)
+        Thread.__init__(self, controllerType)
         self.daemon = True
+        self.controllerType = GPIO.IN if 'IN' else GPIO.OUT  
         self.pins = []
 
     def assignPins(self, pins = []):
         self.pins = pins
         if moduleExists('RPi.GPIO'):
             for pin in pins:
-                RPi.GPIO.setup(pin, RPi.GPIO.IN)
+                GPIO.setup(pin, self.controllerType)
 
     def setPin(self, pin, voltage):
         if not pin in self.pins:
             raise Exception('Pin {0} not set'.format(pin))
 
-        if moduleExists('RPi.GPIO'):
-            RPi.GPIO.output(pin, voltageMaps[voltage])
+        if self.controllerType == 'IN':
+            GPIO.input(pin, voltageMaps[voltage])
+        else:
+            GPIO.output(pin, voltageMaps[voltage])
